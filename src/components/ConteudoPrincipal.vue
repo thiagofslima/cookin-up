d63c6fcd94febcb9428de793de9d61a560755328
Thiago Lima <thiagofslima@gmail.com>
2024-08-08 16:26:49 -0300

    Finalizando. Fazendo busca de receitas

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -32,14 +32,19 @@ export default {
   <main class="conteudo-principal">
     <SuaLista :ingredientes="ingredientes" />
 
-    <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
-        @adicionar-ingrediente="adicionarIngrediente"
-        @remover-ingrediente="removerIngrediente"
-        @buscar-receitas="navegar('MostrarReceitas')"
-    />
+    <KeepAlive include="SelecionarIngredientes" >
+      <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
+          @adicionar-ingrediente="adicionarIngrediente"
+          @remover-ingrediente="removerIngrediente"
+          @buscar-receitas="navegar('MostrarReceitas')"
+      />
 
-    <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'" />
-
+      <MostrarReceitas
+          v-else-if="conteudo === 'MostrarReceitas'"
+          :ingredientes="ingredientes"
+          @editar-receitas="navegar('SelecionarIngredientes')"
+      />
+    </KeepAlive>
 
   </main>
 </template>
