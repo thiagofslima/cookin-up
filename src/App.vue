b1d3fe3f7d5262992ba232951e607f70fee0cfb9
Thiago Lima <thiagofslima@gmail.com>
2024-08-08 08:33:05 -0300

    Criando seleção de ingredientes e importando imagens

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+import SelecionarIngredientes from './components/SelecionarIngredientes.vue';
 
 export default {
-  components: { Banner }
+  components: { Banner, ConteudoPrincipal, SelecionarIngredientes }
 }
 </script>
 
 <template>
   <Banner />
+  <ConteudoPrincipal />
 </template>
